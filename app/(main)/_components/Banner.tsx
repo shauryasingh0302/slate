3b0091321ab5de7ef2f24a2c0f506db536496490
Shaurya Singh <shauryasingh0302@icloud.com>
2026-08-16 23:47:14 +0530

    fix: clean up formatting in Banner component; adjust spacing and structure for improved readability

--- app/(main)/_components/Banner.tsx
+++ app/(main)/_components/Banner.tsx
@@ -3,15 +3,21 @@
 import { Id } from '@/convex/_generated/dataModel'
 import React from 'react'
 
+
 interface BannerProps {
+
     documentId: Id<"documents">
+
 }
 
 const Banner = ({documentId}: BannerProps
     
 ) => {
   return (
-    <div>{}</div>
+
+    <div>{
+      }</div>
+      
   )
 }
 
