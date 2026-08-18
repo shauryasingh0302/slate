00f379cb5a8f8c518b343a2c110fc24fb6eb25cf
Shaurya Singh <shauryasingh0302@icloud.com>
2026-08-18 06:53:58 +0530

    feat: add Blocknote dependencies and create initial editor component
    - Added @blocknote/core and @blocknote/react dependencies to package.json and package-lock.json.
    - Created a new Editor component in components/editor.tsx that utilizes Blocknote for rich text editing.

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Cover from "@/components/cover";
+import Editor from "@/components/editor";
 import Toolbar from "@/components/toolbar";
 import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
@@ -23,17 +24,17 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
 
     if (document === undefined) {
         return (
-          <div>
-            <Cover.Skeleton/>
-            <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
-              <div className="space-y-4 pl-18 pt-4">
-                <Skeleton className="h-14 w-[50%]"/>
-                <Skeleton className="h-4 w-[80%]"/>
-                <Skeleton className="h-4 w-[40%]"/>
-                <Skeleton className="h-4 w-[60%]"/>
-              </div>
+            <div>
+                <Cover.Skeleton />
+                <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
+                    <div className="space-y-4 pl-18 pt-4">
+                        <Skeleton className="h-14 w-[50%]" />
+                        <Skeleton className="h-4 w-[80%]" />
+                        <Skeleton className="h-4 w-[40%]" />
+                        <Skeleton className="h-4 w-[60%]" />
+                    </div>
+                </div>
             </div>
-          </div>
         );
     }
 
@@ -43,9 +44,10 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
 
     return (
         <div className="pb-40">
-            <Cover url={document.coverImage}/>
+            <Cover url={document.coverImage} />
             <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
                 <Toolbar initialData={document} />
+                <Editor onChange={() => {}} initialContent={document.content} />
             </div>
         </div>
     );
---
+++ components/editor.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { BlockNoteEditor, PartialBlock } from "@blocknote/core";
+import { BlockNoteView, useBlockNote } from "@blocknote/react";
+import "@blocknote/core/style.css";
+
+interface EditorProps {
+    onChange: (value: string) => void;
+    initialContent?: string;
+    editable?: boolean;
+}
+
+const Editor = ({ onChange, initialContent, editable }: EditorProps) => {
+    return <div>Editor</div>;
+};
+
+export default Editor;
